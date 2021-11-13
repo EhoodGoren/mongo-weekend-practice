0fd68a63b38fc71e62db035bfd62be5688a72903
EhoodGoren <ehoodgoren@gmail.com>
2021-11-14 00:10:07 +0200

    Added functions to get all comments, get all comments by a specified author, and get all comments of a post.

--- collections-functions/relationships-functions.js
+++ collections-functions/relationships-functions.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const User = require(`${__dirname}/../models/user`);
 const Post = require(`${__dirname}/../models/post`);
 const Comment = require(`${__dirname}/../models/comment`);
+const getPostIdByTitle = require(`${__dirname}/posts-functions`)
 require('dotenv').config({path: `${__dirname}/../.env`});
 
 mongoose.connect(process.env.DATABASE, () => {
@@ -38,3 +39,36 @@ function getPostsByAuthor(author){
 }
 // getPostsByAuthor('GoodGuyGreg');
 // getPostsByAuthor('ScumbagSteve');
+
+function getComments(){
+    Comment.find({})
+    .then(comments => {
+        console.log('Successfully fetched comments');
+        console.log(comments);
+    })
+    .catch(() => console.log('No data'));
+}
+// getComments();
+
+function getCommentsByAuthor(author){
+    Comment.find({username: author})
+    .then(comments => {
+        console.log('Successfully fetched comments');
+        console.log(comments);
+    })
+    .catch(() => console.log('No data'));
+}
+// getCommentsByAuthor('GoodGuyGreg');
+// getCommentsByAuthor('ScumbagSteve');
+
+function getCommentsByPost(post = 'Reports a bug in your code'){
+    getPostIdByTitle(post).then(postId => {
+        Comment.find({post: postId})
+        .then(comments => {
+            console.log('Successfully fetched comments');
+            console.log(comments);
+        })
+        .catch(() => console.log('No data'));
+    })
+}
+// getCommentsByPost();
